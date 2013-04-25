c398afe667c93120081e44c204b6f85906c367a0
macroro@goo.jp <macroro@goo.jp>
2013-04-25 21:57:18 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstes1::Application.config.secret_token = 'a013eac00460a9b5f41c79e3cd5e3ad42b7bb23894b711f4949ac59bfd84ea921c67a92ffd8a789044cf41816eff70b3ab56ccfecba450e90fee7d755ec7b49e'
